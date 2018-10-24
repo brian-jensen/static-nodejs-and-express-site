a865fc50f4689eb226781307ccec71dc750b9786
“Brian” <31598338+brian-jensen@users.noreply.github.com>
2018-10-23 21:12:47 -0700

    built core routes, error handling, and pug templates

--- app.js
+++ app.js
@@ -6,7 +6,22 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 const mainRoutes = require('./routes');
+const projectRoutes = require('./routes/project');
+
 app.use(mainRoutes);
+app.use('/project', projectRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error('Page Not found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error', err);
+});
 
 app.listen(3000, () => {
   console.log('The application is running on localhost:3000');
---
+++ routes/project.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data.json'); 
+const {projects} = data;
+
+router.get('/', (req, res) => {
+  res.redirect(`/project/0`);
+});
+
+router.get('/:id', (req, res) => {
+  let {id} = req.params;
+  let ids = [];
+  projects.forEach(project => ids.push(project.id.toString()));
+
+ if (ids.indexOf(id) !== -1) {
+    res.render('project', {
+      projectName: projects[`${id}`].project_name,
+      description: projects[`${id}`].description,
+      imgOne: projects[`${id}`].image_url[1],
+      imgTwo: projects[`${id}`].image_url[2],
+      gitHub: projects[`${id}`].github_link,
+      technologies: projects[`${id}`].technologies
+    });
+  } else {
+    res.redirect(`/project/0`);
+  }
+  
+});
+
+
+module.exports = router;
